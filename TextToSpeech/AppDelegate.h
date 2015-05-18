4d3f4807d12f5d299ae19b19d9e0be0bb07e0cd6
Green Chen <kuanlin@yahoo-inc.com>
2015-05-18 16:21:35 +0800

    Initial Commit

---
+++ TextToSpeech/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TextToSpeech
+//
+//  Created by Green Chen on 2015/5/18.
+//  Copyright (c) 2015年 com.Yahoo-inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TextToSpeech/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TextToSpeech
+//
+//  Created by Green Chen on 2015/5/18.
+//  Copyright (c) 2015年 com.Yahoo-inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
